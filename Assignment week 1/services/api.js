7cd91e7eb5c1be2581c1016f846a55a6b3e72bcc
Razan-Alahmadi <156917244+Razan-Alahmadi@users.noreply.github.com>
2025-02-23 05:45:49 +0300

    Update api.js

--- Assignment week 1/services/api.js
+++ Assignment week 1/services/api.js
@@ -1,7 +1,8 @@
 export async function fetchCompanyData() {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users");
-        return await response.json();
+        const response = await fetch("https://dummyjson.com/users");
+        const data = await response.json();
+        return data.users;
     } catch (error) {
         throw new Error("Failed to fetch users.");
     }
