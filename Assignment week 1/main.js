4f65eadcae8b4c309567d98d68fb34d8c2dddfa8
Razan-Alahmadi <156917244+Razan-Alahmadi@users.noreply.github.com>
2025-02-23 05:42:49 +0300

    Update main.js

--- Assignment week 1/main.js
+++ Assignment week 1/main.js
@@ -7,6 +7,3 @@ const app = document.getElementById("app");
 const formComponent = new Form();
 formComponent.render(app);
 
-
-const companyListListComponent = new CompanyList();
-companyListListComponent.render(app);
